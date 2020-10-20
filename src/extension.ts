f8edf9b57d31f839c563d37396c97ac81858bac5
fulviocanducci <fulviocanducci@hotmail.com>
2020-10-20 20:08:04 -0300

    Sources

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import { js, jsx, tsx } from './commands';
 
 export function activate(context: vscode.ExtensionContext) {
+  //vscode.commands.executeCommand('setContext', 'inReactJsProject', true);
   const disposables = [...js, ...jsx, ...tsx];
   context.subscriptions.push(...disposables);
 }
